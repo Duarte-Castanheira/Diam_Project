f681ea263e1c1d3a2d0c3469757d074c7f126568
Duarte_Castanheira <dacaa1@iscte-iul.pt>
2025-05-06 15:44:21 +0100

    mudanças

--- sitepr/frontend/src/Header.js
+++ sitepr/frontend/src/Header.js
@@ -7,6 +7,13 @@ function Header() {
                 <a href="/"><img
                     src="/logo.png" alt="Logotipo do clube" style={{ width: '15%', height: 'auto' }}
                 /></a>
+                <a href="/jogos" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Jogos</a>
+            <a href="/equipa" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Equipa</a>
+            <a href="/bilhetes" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Bilhetes</a>
+            <a href="/loja" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Loja</a>
+            <a href="/noticias" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Noticias</a>
+            <a href="/candidatar" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Candidatar</a>
+            <a href="/sobre" style={{ textDecoration: 'none', color: 'black', fontSize: '18px' }}>Sobre</a>
                 
 
             </div>
