993e06063616fb05f526a57ce4d8c9542c8331c1
Afonso_Maia <asmaa1@iscte-iul.pt>
2025-05-13 14:34:39 +0100

    Noticias/imagens no Home

--- sitepr/frontend/src/App.js
+++ sitepr/frontend/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Home from './Home';
 import Forms from './Forms';
 import Header from './Header';
--- sitepr/frontend/src/Home.js
+++ sitepr/frontend/src/Home.js
@@ -1,26 +1,57 @@
-
-import {Container, Row, Col} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function Home() {
-    return (
-        <>
-
-            <Content />
-
-        </>
-       );
-    }
-
-function Content() {
-    return (
-        <Container style={{marginTop: "20px", maxWidth: "800px"}}>
-            <Row>
-                <Col>
-
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <>
+      <Content />
+    </>
+  );
 }
 
-    export default Home;
+function Content() {
+  return (
+    <Container style={{ marginTop: "20px", maxWidth: "400px" }}>
+      <Row>
+        <Col>
+          <Slideshow />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+function Slideshow() {
+  return (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      showThumbs={false}
+      showStatus={false}
+      interval={5000}
+      transitionTime={500}
+    >
+      <div>
+        <img
+          src="/NoticiaJovem.png"
+          alt="Imagem 1"
+        />
+      </div>
+      <div>
+        <img
+          src="/Sub17.png"
+          alt="Imagem 2"
+        />
+      </div>
+      <div>
+        <img
+          src="/RenovacaoEstadio.png"
+          alt="Imagem 3"
+        />
+      </div>
+    </Carousel>
+  );
+}
+
+export default Home;
